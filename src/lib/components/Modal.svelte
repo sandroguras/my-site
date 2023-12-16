1f991d601cdfdd789b0330c856f8e52de98a8213
David Guras <david.guras@pm.me>
2023-12-16 10:52:33 +0200

    Add Modal component
    * add Modal component
    * update packages

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,19 @@
+<div id="review-1" class="popup mfp-with-anim mfp-hide">
+	<div class="row">
+		<div class="col-12 col-sm-2 full-rewiew-con-avatar">
+			<figure class="box box-avatar">
+				<img src="assets/images/avatar-1.png" alt="Daniel Lewis" />
+			</figure>
+			<div class="review-icon-quote"></div>
+		</div>
+		<div class="col-12 col-sm-10 full-rewiew-con-text">
+			<h2 class="title title--h2 mb-1">Daniel Lewis</h2>
+			<span class="review-date">14 June, 2021</span>
+			<p>Test</p>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+  @import "../../styles/app/popup";
+</style>
