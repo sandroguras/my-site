7f3400760c869b3e2d159ddb03c52ef6fee7b5c9
David Guras <david.guras@pm.me>
2024-01-15 14:46:03 +0200

    Add popup modal to Carousel
    * describe popup modal logic
    * describe carousel logic
    * style popup modal
    * remove redundant carousel components

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { register } from 'swiper/element/bundle';
+	import Modal from '$lib/components/Modal.svelte';
+	import type { ReviewData as ReviewDataType, Client as ClientType } from '../../types/types';
 
-	export let swiperId: string = 'swiper-clients';
+	export let swiperId: string = '';
 	export let slidesPerView: number | string = 'auto';
 	export let spaceBetween: number = 30;
 	export let speed: number = 500;
@@ -10,47 +12,12 @@
 	export let pagination: object = {
 		clickable: true
 	};
-	export let breakpoints: object = {
-		320: { slidesPerView: 2, spaceBetween: 0 },
-		580: { slidesPerView: 3, spaceBetween: 30 },
-		991: { slidesPerView: 4, spaceBetween: 30 }
-	};
-	export let injectStylesUrls: string = ['/styles/swiper-pagination.css'];
-	export let slides: object[] = [
-		{
-			link: 'https://greymass.com/',
-			logo: '/images/clients/greymass-color.svg',
-			logo_alt: 'Greymass logo'
-		},
-		{
-			link: 'https://bullish.com',
-			logo: '/images/clients/bullish-color.svg',
-			logo_alt: 'Bullish logo'
-		},
-		{
-			link: 'https://b1.com',
-			logo: '/images/clients/b1-color.svg',
-			logo_alt: 'b1 logo'
-		},
-		{
-			link: 'https://about.voice.com/company/',
-			logo: '/images/clients/voice-color.svg',
-			logo_alt: 'Voice logo'
-		},
-		{
-			link: 'https://eos.io',
-			logo: '/images/clients/eosio-color.svg',
-			logo_alt: 'eosio logo'
-		},
-		{
-			link: 'https://grafic.agency',
-			logo: '/images/clients/grafik-color.svg',
-			logo_alt: 'Grafic logo'
-		}
-	];
+	export let breakpoints: object = {};
+	export let injectStylesUrls: string[] = ['/styles/swiper-pagination.css'];
+	export let slides: (ReviewDataType | ClientType)[] = [];
 
 	// Swiper initialization and custom style injection
-    onMount(() => {
+	onMount(() => {
 		register();
 
 		const swiperEl = document.querySelector(`#${swiperId}`);
@@ -61,44 +28,80 @@
 			grabCursor,
 			pagination,
 			breakpoints,
-			injectStylesUrls
+			injectStylesUrls,
+			loop: true
 		};
 
 		Object.assign(swiperEl, params);
 		swiperEl.initialize();
 	});
+
+	// Type guard function to check if a slide is of type ReviewDataType
+	function isReviewData(slide: ReviewDataType | ClientType): slide is ReviewDataType {
+		return 'short_copy' in slide;
+	}
+
+	// Modal setup
+	let showModal: boolean = false; // Boolean to show/hide the modal
+	let modalData: ReviewDataType; // Object to hold data of the clicked carousel item
+	let modalIndex: number; // Index of the clicked carousel item
+
+	function openModal(index: number): void {
+		const slide = slides[index];
+		if (isReviewData(slide)) {
+			showModal = true;
+			modalData = slide;
+			modalIndex = index;
+		}
+	}
+
+	function closeModal(): void {
+		showModal = false;
+	}
 </script>
 
 {#if swiperId === 'swiper-clients'}
 	<swiper-container id="swiper-clients" init="false">
 		<!-- Client links -->
 		{#each slides as slide}
-			<swiper-slide class="js-carousel-clients">
-				<figure class="swiper-slide">
-					<a href={slide.link} target="_blank"
-						><img class="logo-client" src={slide.logo} alt={slide.logo_alt} /></a
-					>
-				</figure>
-			</swiper-slide>
+			{#if 'logo' in slide}
+				<!-- Check if the slide is a Client -->
+				<swiper-slide class="js-carousel-clients">
+					<figure class="swiper-slide">
+						<a href={slide.link} target="_blank">
+							<img class="logo-client" src={slide.logo} alt={slide.logo_alt} />
+						</a>
+					</figure>
+				</swiper-slide>
+			{/if}
 		{/each}
 	</swiper-container>
+
+	<style lang="scss">
+		@import '../../styles/app/clients';
+	</style>
 {/if}
+
 {#if swiperId === 'swiper-testimonials'}
 	<swiper-container id="swiper-testimonials" init="false">
 		<!-- Testimonials -->
-		{#each slides as slide}
-			<swiper-slide class="review-item box box-inner">
-				<figure class="box box-avatar">
-					<img src={slide.image} alt={slide.image_alt} />
-				</figure>
-				<h4 class="title title--h3">{slide.name}</h4>
-				<p class="review-item__caption">{slide.copy}</p>
-			</swiper-slide>
+		{#each slides as slide, i}
+			{#if 'short_copy' in slide}
+				<!-- Check if the slide is a ReviewData -->
+				<swiper-slide class="review-item box box-inner" on:click={() => openModal(i)}>
+					<figure class="box box-avatar">
+						<img src={slide.image} alt={slide.image_alt} />
+					</figure>
+					<h4 class="title title--h3">{slide.name}</h4>
+					<p class="review-item__caption">{slide.short_copy}</p>
+				</swiper-slide>
+			{/if}
 		{/each}
 	</swiper-container>
-{/if}
 
-<style lang="scss">
-	@import '../../styles/app/clients';
-	@import "../../styles/app/testimonials";
-</style>
+	<style lang="scss">
+		@import '../../styles/app/testimonials';
+	</style>
+
+	<Modal {modalData} {modalIndex} {showModal} {closeModal} />
+{/if}
--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,19 +1,76 @@
-<div id="review-1" class="popup mfp-with-anim mfp-hide">
-	<div class="row">
-		<div class="col-12 col-sm-2 full-rewiew-con-avatar">
-			<figure class="box box-avatar">
-				<img src="assets/images/avatar-1.png" alt="Daniel Lewis" />
-			</figure>
-			<div class="review-icon-quote"></div>
-		</div>
-		<div class="col-12 col-sm-10 full-rewiew-con-text">
-			<h2 class="title title--h2 mb-1">Daniel Lewis</h2>
-			<span class="review-date">14 June, 2021</span>
-			<p>Test</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { scale } from 'svelte/transition';
+	import { backOut } from 'svelte/easing';
+	import type { ReviewData as ReviewDataType } from '../../types/types';
+	export let showModal: boolean = false;
+	export let modalData: ReviewDataType;
+	export let modalIndex: number;
+	export let closeModal: () => void;
+	let originalOverflow: string;
+
+	function formatDate(dateStr: string): string {
+		const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' };
+		const date = new Date(dateStr);
+		return new Intl.DateTimeFormat('en-US', options).format(date);
+	}
+
+	onMount(() => {
+		if (browser) {
+			// Only run this code on the client side
+			originalOverflow = document.body.style.overflow;
+		}
+
+		return () => {
+			if (browser) {
+				// Cleanup code for client side
+				document.body.style.overflow = originalOverflow;
+			}
+		};
+	});
+
+	$: if (browser && showModal) {
+		document.body.style.overflow = 'hidden';
+	} else if (browser) {
+		document.body.style.overflow = originalOverflow;
+	}
+</script>
+
+{#if showModal}
+	<div class="mfp-bg mfp-zoom-out mfp-ready"></div>
+	<div
+		class="mfp-wrap mfp-close-btn-in mfp-auto-cursor mfp-zoom-out mfp-ready"
+		tabindex="-1"
+		style="overflow: hidden auto;"
+	>
+		<div class="mfp-container mfp-s-ready mfp-inline-holder">
+			<div class="mfp-content">
+				<div transition:scale={{ duration: 500, easing: backOut }}>
+					<div id="review-{modalIndex}" class="popup">
+						<div class="row">
+							<div class="col-12 col-sm-2 full-rewiew-con-avatar">
+								<figure class="box box-avatar">
+									<img src={modalData.image} alt={modalData.image_alt} />
+								</figure>
+								<div class="review-icon-quote"></div>
+							</div>
+							<div class="col-12 col-sm-10 full-rewiew-con-text">
+								<h2 class="title title--h2 mb-1">{modalData.name}</h2>
+								<span class="review-date">{formatDate(modalData.review_date)}</span>
+								{modalData.copy}
+							</div>
+						</div>
+						<button title="Close (Esc)" type="button" class="mfp-close" on:click={closeModal}
+							>×</button
+						>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
 
 <style lang="scss">
-  @import "../../styles/app/popup";
+	@import '../../styles/app/popup';
 </style>
