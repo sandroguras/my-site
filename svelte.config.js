e8dd2655e85fb10b6d174477c1acf002a85a1c1a
Guras <sandro.guras@pm.me>
2024-02-27 07:42:52 -0500

    Tweak alias setup to be processed by sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,13 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
+		alias: {
+			'#types': './src/types'
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	vite: {
-		resolve: {
-			alias: {
-				'#types': './src/types'
-			}
-		}
 	}
 };
 
