9e67924bce157f5f41d639bce7dfb4458f4bfd12
David Guras <david.guras@pm.me>
2024-03-10 18:47:11 -0400

    Isolate Form component on page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,11 +3,4 @@
 	import Form from '$lib/components/Form/Form.svelte';
 </script>
 
-<div class="map" id="map"></div>
-<h2 class="title title--h2">Contact Form</h2>
-
 <Form />
-
-<style lang="scss">
-	@import '#styles/app/map';
-</style>
