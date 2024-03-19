88de7a022e38eec92a189d87414a4f226f6cfad8
Guras <sandro.guras@pm.me>
2024-03-19 17:02:04 -0400

    Change open graph

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -13,9 +13,13 @@
 <meta name="description" content={description} />
 
 <!-- OpenGraph meta tags -->
+<meta property="og:site_name" content={title} />
+<meta property="og:locale" content="en_US" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
 <meta property="og:title" content={title} />
 <meta property="og:type" content="website" />
-<meta property="og:image" content={`${siteURL}/opengraph.png`} />
+<meta property="og:image" content={`${siteURL}/opengraph.jpg`} />
 <meta property="og:url" content={siteURL} />
 <meta property="og:description" content={description} />
 
@@ -24,4 +28,5 @@
 <meta property="twitter:url" content={siteURL} />
 <meta property="twitter:title" content={title} />
 <meta property="twitter:description" content={description} />
-<meta property="twitter:image" content={`${siteURL}/opengraph.png`} />
+<meta property="twitter:image" content={`${siteURL}/opengraph.jpg`} />
+<meta name="twitter:card" content="summary_large_image" />
