f23aadad4e68df07352b48ae685c7058d75dc89a
Guras <sandro.guras@pm.me>
2024-03-25 13:41:47 -0400

    Tweak active link logic work with second-degree links
    * modify the logic so that "active" class is applied only to first-degree links (e.g., /portfolio activates the portfolio link, and /portfolio/portfolio-item-1 also activates the portfolio link, but not deeper nested links)

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,12 +3,25 @@
 	import { page } from '$app/stores';
 
 	export let links: NavLinkType[] = [];
+
+	// Function to determine if the link is active
+	// considering only the first-degree path segment
+	function isActiveLink(currentPath: string, linkPath: string): boolean {
+		// Split the paths into segments
+		const currentSegments = currentPath.split('/').filter(Boolean);
+		const linkSegments = linkPath.split('/').filter(Boolean);
+
+		// Check if the first segment of the current path matches the first segment of the link path
+		return linkSegments.length === 0 ? currentPath === linkPath : currentSegments[0] === linkSegments[0];
+	}
 </script>
 
 <div class="nav-container">
 	<ul class="nav">
 		{#each links as link}
-			<li class="nav__item"><a class:active={$page.url.pathname === link.path} href="{link.path}">{link.label}</a></li>
+			<li class="nav__item">
+				<a class:active={isActiveLink($page.url.pathname, link.path)} href="{link.path}">{link.label}</a>
+			</li>
 		{/each}
 	</ul>
 </div>
