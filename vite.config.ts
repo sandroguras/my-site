c6fed0cffd21a857e3f528f525957e8a4385b556
Guras <sandro.guras@pm.me>
2024-03-27 13:49:29 -0400

    Style refactoring
    * move g-font declaration from vars.scss to app.html
    * app.scss author/version tags update
    * remove en.ts
    * rename variables.scss to global.scss

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
+	plugins: [sveltekit()],
 
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: "@use \"src/variables.scss\" as *;"
+                additionalData: "@use \"src/global.scss\" as *;"
             }
         }
     }
