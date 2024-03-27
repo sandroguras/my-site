cf55f094892a853e74f04a116f48b66069737722
Guras <sandro.guras@pm.me>
2024-03-26 22:32:25 -0400

    Edit EOSIO copy

--- src/lib/data/portfolioData.ts
+++ src/lib/data/portfolioData.ts
@@ -4,7 +4,8 @@ const mockData: PortfolioProject[] = [
 	{
 		slug: 'bullish',
 		listName: 'bullish.com',
-		pageTitle: '<a href="https://bullish.com" target="_blank" rel="noopener noreferrer" aria-label="View bullish.com website">bullish.com</a>',
+		pageTitle:
+			'<a href="https://bullish.com" target="_blank" rel="noopener noreferrer" aria-label="View bullish.com website">bullish.com</a>',
 		coverImage: '/images/portfolio/bullish/bullish-cover-image.webp',
 		coverImageAlt: 'bullish.com cover image',
 		thumbnail: '/images/portfolio/bullish/bullish-thumb.webp',
@@ -55,16 +56,18 @@ const mockData: PortfolioProject[] = [
 				alt: 'Bullish website top 5 trading venue section'
 			}
 		],
-		description: "<p>The <a href=\"https://bullish.com\" target=\"_blank\" rel=\"noopener noreferrer\" aria-label=\"View bullish.com website\">bullish.com</a> website project was focused on developing a marketing platform for a cryptocurrency exchange. As the web director, I spearheaded the project from its inception, taking charge of the architecture planning and initial code layout. The initial phase began with a simple content management system (CMS), which I helped scale into a more comprehensive web multi-system to meet the specialized needs of marketing a crypto exchange. I worked closely with the team to design a flexible system that allowed the marketing team to swiftly adapt content, launch new campaigns, and introduce pages that could engage and inform users about the exchange's features and benefits. This level of adaptability was crucial for staying competitive in the fast-paced world of cryptocurrency trading.</p>\n" +
-			"\n" +
-			"<p>In my role as the project lead, I oversaw a team of six developers located in different regions. This setup presented unique challenges in terms of coordination and maintaining a cohesive workflow. Our key objectives were ensuring timely delivery, managing stakeholder expectations, and driving the development process forward while adhering to high coding standards. To meet these goals, I focused on efficient project management practices, such as using Jira for task tracking and fostering a culture of regular code reviews to maintain code quality and facilitate feature development.</p>\n" +
-			"\n" +
-			"<p>The security aspect of the site was particularly important, given the sensitive nature of the financial information and transactions associated with a crypto exchange. From the early stages of architecture planning, I prioritized the implementation of stringent security protocols, balancing these with the need for a high-performing website. This required a thoughtful approach to coding practices and a commitment to continuous testing and improvement. The project was not just about building a marketing site; it was about creating a secure, efficient platform that could effectively promote the crypto exchange and build trust with its users. Through teamwork and a commitment to best practices, we developed a site that not only met but exceeded these objectives.</p>"
+		description:
+			'<p>The <a href="https://bullish.com" target="_blank" rel="noopener noreferrer" aria-label="View bullish.com website">bullish.com</a> website project was focused on developing a marketing platform for a cryptocurrency exchange. As the web director, I spearheaded the project from its inception, taking charge of the architecture planning and initial code layout. The initial phase began with a simple content management system (CMS), which I helped scale into a more comprehensive web multi-system to meet the specialized needs of marketing a crypto exchange. I worked closely with the team to design a flexible system that allowed the marketing team to swiftly adapt content, launch new campaigns, and introduce pages that could engage and inform users about the exchange\'s features and benefits. This level of adaptability was crucial for staying competitive in the fast-paced world of cryptocurrency trading.</p>\n' +
+			'\n' +
+			'<p>In my role as the project lead, I oversaw a team of six developers located in different regions. This setup presented unique challenges in terms of coordination and maintaining a cohesive workflow. Our key objectives were ensuring timely delivery, managing stakeholder expectations, and driving the development process forward while adhering to high coding standards. To meet these goals, I focused on efficient project management practices, such as using Jira for task tracking and fostering a culture of regular code reviews to maintain code quality and facilitate feature development.</p>\n' +
+			'\n' +
+			'<p>The security aspect of the site was particularly important, given the sensitive nature of the financial information and transactions associated with a crypto exchange. From the early stages of architecture planning, I prioritized the implementation of stringent security protocols, balancing these with the need for a high-performing website. This required a thoughtful approach to coding practices and a commitment to continuous testing and improvement. The project was not just about building a marketing site; it was about creating a secure, efficient platform that could effectively promote the crypto exchange and build trust with its users. Through teamwork and a commitment to best practices, we developed a site that not only met but exceeded these objectives.</p>'
 	},
 	{
 		slug: 'eosio',
 		listName: 'eos.io',
-		pageTitle: '<a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a>',
+		pageTitle:
+			'<a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a>',
 		coverImage: '/images/portfolio/eosio/eosio-cover-image.webp',
 		coverImageAlt: 'eos.io cover image',
 		thumbnail: '/images/portfolio/eosio/eosio-thumb.webp',
@@ -115,14 +118,21 @@ const mockData: PortfolioProject[] = [
 				alt: 'EOSIO website Why EOSIO slider section'
 			}
 		],
-		description:
-			'<p>EOSIO is a blockchain protocol that enables horizontal scaling of decentralized applications, allowing developers to efficiently create high performance applications.</p>' +
-			'<p>The website provides information about the protocol and its features.</p>'
+		description: '<p>As the web director for the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> website project, I played a key role in overseeing the site\'s development and evolution throughout its four-year lifecycle. The project aimed to create a robust marketing platform for the EOS blockchain, a cutting-edge, open-source platform renowned for its industry-leading transaction speed and versatile utility. <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> was engineered to support enterprise-grade use cases and cater to both public and private blockchain deployments.</p>\n' +
+			'\n' +
+			'<p>I spearheaded the team through three significant visual iterations of the site, each designed to optimize user experience and align with the evolving needs of the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> platform. My team and I diligently supported the ongoing marketing campaigns and site extensions necessary to maintain the platform\'s competitiveness in the rapidly advancing world of blockchain technology.</p>\n' +
+			'\n' +
+			'<p>One of the key features I directed was the development of a powerful section constructor system. This system enabled marketing professionals and content editors to swiftly scaffold new pages on the fly, providing them with the agility to adapt to the industry\'s ever-changing demands. Additionally, I implemented a 3-stage deployment system that allowed for seamless deployments and staging of information, which decreased deploy speeds to mere seconds. This feature was pivotal in ensuring that the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> site remained responsive and nimble in delivering pertinent content to its users.</p>\n' +
+			'\n' +
+			'<p>Given the delicate nature of the blockchain industry, security was of paramount importance. I prioritized maintaining a robust security framework for the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> site. My team and I implemented rigorous security measures and regularly conducted audits to identify and address any potential vulnerabilities. We collaborated closely with the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> security team to ensure that the site met the highest standards of data protection and user privacy.</p>\n' +
+			'\n' +
+			'<p>The <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> website project demonstrated my ability to lead a team in delivering a high-performing, secure, and user-centric platform. By combining my technical expertise with strong leadership skills, I successfully navigated the complexities of the project and delivered a website that effectively promoted the <a href="https://eos.io" target="_blank" rel="noopener noreferrer" aria-label="View eosio website">eos.io</a> blockchain platform and positioned it as a leader in the industry.</p>'
 	},
 	{
 		slug: 'blockone',
 		listName: 'block.one',
-		pageTitle: '<a href="https://b1.com" target="_blank" rel="noopener noreferrer" aria-label="View block.one website">b1.com</a>',
+		pageTitle:
+			'<a href="https://b1.com" target="_blank" rel="noopener noreferrer" aria-label="View block.one website">b1.com</a>',
 		coverImage: '/images/portfolio/b1/b1-cover-image.webp',
 		coverImageAlt: 'block.one cover image',
 		thumbnail: '/images/portfolio/b1/b1-thumb.webp',
@@ -163,7 +173,7 @@ const mockData: PortfolioProject[] = [
 			{
 				src: '/images/portfolio/b1/b1.com_screen_6.webp',
 				alt: 'b1 website contact page'
-			},
+			}
 		],
 		description:
 			'<p>Block.one is a software company specializing in high performance blockchain technologies.</p>' +
@@ -172,7 +182,8 @@ const mockData: PortfolioProject[] = [
 	{
 		slug: 'voice',
 		listName: 'voice.com',
-		pageTitle: '<a href="https://voice.com" target="_blank" rel="noopener noreferrer" aria-label="View voice.com website">voice.com</a>',
+		pageTitle:
+			'<a href="https://voice.com" target="_blank" rel="noopener noreferrer" aria-label="View voice.com website">voice.com</a>',
 		coverImage: '/images/portfolio/voice/voice-cover-image.webp',
 		coverImageAlt: 'voice.com cover image',
 		thumbnail: '/images/portfolio/voice/voice-thumb.webp',
@@ -221,7 +232,7 @@ const mockData: PortfolioProject[] = [
 			{
 				src: '/images/portfolio/voice/voice.com_screen_8.webp',
 				alt: 'Voice.com website'
-			},
+			}
 		],
 		description:
 			'<p>Voice is a blockchain-based social media platform that redefines social media by putting control back into the hands of users. The website provides information about the platform and its features.</p>'
