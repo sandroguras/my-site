1a16a2a92ca76c9c2007bc312616a4325848654f
David Guras <david.guras@pm.me>
2024-03-31 11:53:42 -0400

    Establish sing blog post page

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,39 @@
+<div class="pb-3">
+	<header class="header-post">
+		<h1 class="title title--h1">Web Design Weekly #38</h1>
+		<div class="caption-post">
+			<p>Above all, think of life as a prototype. We can conduct experiments, make discoveries, and change our perspectives. We can look for opportunities to turn processes into projects that have tangible outcomes.</p>
+		</div>
+		<div class="header-post__image-wrap">
+			<img class="cover lazyload" src="assets/images/958x400.jpg" alt="" />
+		</div>
+	</header>
+	<div class="caption-post">
+		<p>We can learn how to take joy in the things we create whether they take the form of a fleeting experience or an heirloom that will last for generations. We can learn that reward comes in creation and re-creation.</p>
+		<p>It is not enough that we build products that function, that are understandable and usable, we also need to build products that bring joy and excitement, pleasure and fun, and, yes, beauty to people’s lives. Creativity is to discover a question that has never been asked.</p>
+	</div>
+	<div class="gallery-post">
+		<img class="gallery-post__item cover lazyload" src="/images/400x550.jpg" data-zoom alt="" />
+		<img class="gallery-post__item cover lazyload" src="/images/400x550.jpg" data-zoom alt="" />
+		<div class="gallery-post__caption">Work by <a href="#">Colin Adams</a>, <a href="#">Mark Craig</a></div>
+	</div>
+	<div class="caption-post">
+		<h2 class="title title--h2">If you’re not prepared to be wrong, you’ll never come up with anything original.</h2>
+		<p>Here is one of the few effective keys to the design problem: the ability of the designer to recognize as many of the constraints as possible his willingness and enthusiasm for working within these constraints. The most profound technologies are those that disappear. They weave themselves into the fabric of everyday life until they are indistinguishable from it.</p>
+		<blockquote class="block-quote">
+			<p>The alternative to good design is always bad design. There is no such thing as no design.</p>
+			<span class="block-quote__author">Adam Judge</span>
+		</blockquote>
+		<p>Above all, think of life as a prototype. We can conduct experiments, make discoveries, and change our perspectives. We can look for opportunities to turn processes into projects that have tangible outcomes.</p>
+		<p>We can learn how to take joy in the things we create whether they take the form of a fleeting experience or an heirloom that will last for generations. We can learn that reward comes in creation and re-creation, no just in the consumption of the world around us. Active participation in the process of creation is our right and our privilege. We can learn to measure the success of our ideas not by our bank accounts by their impact on the world.</p>
+	</div>
+	<footer class="footer-post">
+		<a class="footer-post__share" href="http://facebook.com"><i class="font-icon icon-facebook"></i><span>Facebook</span></a>
+		<a class="footer-post__share" href="http://twitter.com"><i class="font-icon icon-twitter"></i><span>Twitter</span></a>
+		<a class="footer-post__share" href="http://linkedin.com"><i class="font-icon icon-linkedin2"></i><span>Linkedin</span></a>
+	</footer>
+</div>
+
+<style lang="scss">
+	@import "#styles/app/single-post";
+</style>
