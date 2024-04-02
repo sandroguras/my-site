3ea4d1c4dcbecba4cfd02683e9ab74e7c40347b5
Guras <sandro.guras@pm.me>
2024-04-02 11:58:03 -0400

    Refactor Portfolio interface
    * abstract image data into separate interface

--- src/routes/portfolio/+page.ts
+++ src/routes/portfolio/+page.ts
@@ -2,14 +2,13 @@ import type { PageLoad } from './$types';
 import { fetchAllProjects } from '$lib/data/portfolioData';
 
 export const load: PageLoad = async () => {
-	const allProjects = await fetchAllProjects() || [];
+	const allProjects = (await fetchAllProjects()) || [];
 
 	// Map the full project objects to only include the properties needed by the component
-	const projects = allProjects.map(({ slug, listName, thumbnail, thumbnailAlt }) => ({
+	const projects = allProjects.map(({ slug, listName, thumb }) => ({
 		slug,
 		listName,
-		thumbnail,
-		thumbnailAlt
+		thumb
 	}));
 
 	return { projects };
