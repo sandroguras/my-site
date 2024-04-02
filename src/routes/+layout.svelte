011f17524dc682b9cf61f40607d4d28346d5bf31
Guras <sandro.guras@pm.me>
2024-04-02 18:33:04 -0400

    Add scroll indicator component with conditional logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-  import "../app.scss";
+	import '../app.scss';
+	import { page } from '$app/stores';
 	import type { NavLink as NavLinkType } from '#types/NavLink';
-  import SEO from "$lib/components/SEO.svelte";
-  import NavBar from "$lib/components/NavBar.svelte";
-  import Sidebar from "$lib/components/Sidebar.svelte";
-  import PageTitle from '$lib/components/PageTitle.svelte'
+	import SEO from '$lib/components/SEO.svelte';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import PageTitle from '$lib/components/PageTitle.svelte';
+	import ScrollIndicator from '$lib/components/ScrollIndicator.svelte';
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 	inject({ mode: dev ? 'development' : 'production' });
 
 	let links: NavLinkType[] = [
-		{ path: '/', label: 'About'},
-		{ path: '/resume', label: 'Resume'},
-		{ path: '/portfolio', label: 'Portfolio'},
-		{ path: '/blog', label: 'Blog'},
-		{ path: '/contact', label: 'Contact'},
-	]
+		{ path: '/', label: 'About' },
+		{ path: '/resume', label: 'Resume' },
+		{ path: '/portfolio', label: 'Portfolio' },
+		{ path: '/blog', label: 'Blog' },
+		{ path: '/contact', label: 'Contact' }
+	];
+
+	$: isBlogSubpage = $page.url.pathname.startsWith('/blog/');
+
 </script>
 <svelte:head>
 	<SEO />
 </svelte:head>
 <body>
+{#if isBlogSubpage}
+	<ScrollIndicator />
+{/if}
 <main class="main">
 	<div class="container gutter-top gutter-bottom">
 		<div class="row sticky-parent">
