63335feb3e175efcfe8e98da9cb8fef0c934442e
Guras <sandro.guras@pm.me>
2024-04-03 16:40:26 -0400

    Refactor SEO logic
    * add switch statement logic for og, title, description
    * add additional og for pages
    * add titles and descriptions for pages
    * change opengraph directory to `images/opengraphs`

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -1,27 +1,66 @@
 <script lang="ts">
 	//TODO need to improve wording and test out the meta tags
 	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
 
-	const siteURL = "https://davidguras.dev";
 
-	const title: string = 'David Guras | Precision Web Engineering';
-	const description: string = 'David Guras: Expert Web Director, crafting large-scale, precise solutions. Ensures fast, responsive, accessible projects with unmatched quality.';
+	const siteURL = "https://davidguras.dev/";
+
+	const defaultTitle: string = 'David Guras | Precision Web Engineering';
+	const defatulSiteName: string = 'David Guras';
+	const defaultDescription: string = 'David Guras: Expert Web Director, crafting large-scale, precise solutions. Ensures fast, responsive, accessible projects with unmatched quality.';
+	const defaultImage = `${siteURL}images/opengraphs/default-og.jpg`;
+
+	const metadata = derived(page, $page => {
+		switch ($page.url.pathname) {
+			case '/resume':
+				return {
+					title: 'David Guras | My Resume',
+					description: 'View my professional resume and career accomplishments.',
+					image: `${siteURL}images/opengraphs/resume-og.jpg`
+				};
+			case '/portfolio':
+				return {
+					title: 'David Guras | Portfolio',
+					description: 'Explore my portfolio of web development projects.',
+					image: `${siteURL}images/opengraphs/portfolio-og.jpg`
+				};
+			case '/blog':
+				return {
+					title: 'David Guras | Blog',
+					description: 'Dive into the latest trends, tips, and personal insights in web development and engineering. Stay updated with David Guras\'s blog.',
+					image: `${siteURL}images/opengraphs/blog-og.jpg`
+				};
+			case '/contact':
+				return {
+					title: 'David Guras | Contact Me',
+					description: 'Have questions, project ideas, or want to say hello? Don’t hesitate to get in touch. I’m always here to discuss new opportunities and collaborations.',
+					image: `${siteURL}images/opengraphs/blog-og.jpg`
+				};
+			default:
+				return {
+					title: defaultTitle,
+					description: defaultDescription,
+					image: defaultImage
+				};
+		}
+	});
 </script>
 
 <!-- Various meta tags and link elements -->
-<title>{title}</title>
-<meta name="description" content={description} />
+<title>{$metadata.title}</title>
+<meta name="description" content={$metadata.description} />
 
 <!-- OpenGraph meta tags -->
-<meta property="og:title" content={title} />
-<meta property="og:description" content={description} />
-<meta property="og:url" content={siteURL} />
+<meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
-<meta property="og:image" content={`${siteURL}/opengraph.jpg`} />
+<meta property="og:url" content={siteURL} />
+<meta property="og:title" content={$metadata.title} />
+<meta property="og:site_name" content={defatulSiteName} />
+<meta property="og:description" content={$metadata.description} />
+<meta property="og:image" content={$metadata.image} />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:site_name" content={title} />
 <link rel="canonical" href="{$page.url.href}" />
 
 
