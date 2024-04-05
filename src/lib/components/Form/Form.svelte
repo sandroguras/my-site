61ea37cd7080ede7182d9083f0b30c8825168ffb
Guras <sandro.guras@pm.me>
2024-04-05 18:37:40 -0400

    Add hCaptcha type package

--- src/lib/components/Form/Form.svelte
+++ src/lib/components/Form/Form.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { slide } from 'svelte/transition';
 	import { sineOut } from 'svelte/easing';
+	import '@hcaptcha/types';
 	import DOMPurify from 'dompurify';
 	import Button from '$lib/components/Button.svelte';
 
