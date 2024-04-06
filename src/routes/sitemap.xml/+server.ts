b093cb98124cfb4e8a237efb46b8e10bee398471
Guras <sandro.guras@pm.me>
2024-04-06 08:13:10 -0400

    Change protocol to https in sitemap script

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -25,7 +25,7 @@ export const GET: RequestHandler = async ({ request }) => {
 		];
 
 		const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
 	.map(
 		(page) =>
